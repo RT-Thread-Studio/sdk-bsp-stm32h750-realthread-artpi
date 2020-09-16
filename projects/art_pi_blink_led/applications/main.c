260ca7cce573c396b99d4e2df7ef917188984357
tangweikang <tangweikang@rt-thread.com>
2020-09-16 15:52:37 +0800

    [linkscript] update link script address

--- projects/art_pi_blink_led/applications/main.c
+++ projects/art_pi_blink_led/applications/main.c
@@ -29,3 +29,14 @@ int main(void)
     }
     return RT_EOK;
 }
+
+#include "stm32h7xx.h"
+static int vtor_config(void)
+{
+    /* Vector Table Relocation in Internal QSPI_FLASH */
+    SCB->VTOR = QSPI_BASE;
+    return 0;
+}
+INIT_BOARD_EXPORT(vtor_config);
+
+
