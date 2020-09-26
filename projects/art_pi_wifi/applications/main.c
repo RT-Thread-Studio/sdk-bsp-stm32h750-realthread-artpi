a8561783f8acb18e4bd0f1b73251e8f043e4269d
armink <armink.ztl@gmail.com>
2020-09-26 11:36:18 +0800

    【增加】wifi 例程。
    Signed-off-by: armink <armink.ztl@gmail.com>

---
+++ projects/art_pi_wifi/applications/main.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-09-02     RT-Thread    first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include "drv_common.h"
+
+#define LED_PIN GET_PIN(I, 8)
+
+extern void wlan_autoconnect_init(void);
+
+int main(void)
+{
+    rt_uint32_t count = 1;
+    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
+
+    /* init Wi-Fi auto connect feature */
+    wlan_autoconnect_init();
+    /* enable auto reconnect on WLAN device */
+    rt_wlan_config_autoreconnect(RT_TRUE);
+
+    while(count++)
+    {
+        rt_thread_mdelay(500);
+        rt_pin_write(LED_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED_PIN, PIN_LOW);
+    }
+    return RT_EOK;
+}
+
+#include "stm32h7xx.h"
+static int vtor_config(void)
+{
+    /* Vector Table Relocation in Internal QSPI_FLASH */
+    SCB->VTOR = QSPI_BASE;
+    return 0;
+}
+INIT_BOARD_EXPORT(vtor_config);
+
+
