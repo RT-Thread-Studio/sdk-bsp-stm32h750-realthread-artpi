13849d3b8b34bd3376c01066f20aeff618589cf4
malongwei <2544047213@qq.com>
2020-10-12 23:30:50 +0800

    更改mpu,添加web_root文件夹

--- projects/industry_io_gateway/board/drv_mpu.c
+++ projects/industry_io_gateway/board/drv_mpu.c
@@ -24,10 +24,10 @@ int mpu_init(void)
     MPU_InitStruct.Size              = MPU_REGION_SIZE_512KB;
     MPU_InitStruct.AccessPermission  = MPU_REGION_FULL_ACCESS;
     MPU_InitStruct.IsBufferable      = MPU_ACCESS_NOT_BUFFERABLE;
-    MPU_InitStruct.IsCacheable       = MPU_ACCESS_NOT_CACHEABLE;
+    MPU_InitStruct.IsCacheable       = MPU_ACCESS_CACHEABLE;
     MPU_InitStruct.IsShareable       = MPU_ACCESS_NOT_SHAREABLE;
     MPU_InitStruct.Number            = MPU_REGION_NUMBER0;
-    MPU_InitStruct.TypeExtField      = MPU_TEX_LEVEL1;
+    MPU_InitStruct.TypeExtField      = MPU_TEX_LEVEL0;
     MPU_InitStruct.SubRegionDisable  = 0X00;
     MPU_InitStruct.DisableExec       = MPU_INSTRUCTION_ACCESS_ENABLE;
 
@@ -66,22 +66,6 @@ int mpu_init(void)
     MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
 
     HAL_MPU_ConfigRegion(&MPU_InitStruct);
-  
-    /* Configure the MPU attributes as Cacheable write through 
-       for LwIP RAM heap which contains the Tx buffers */
-    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-    MPU_InitStruct.BaseAddress = 0x30044000;
-    MPU_InitStruct.Size = MPU_REGION_SIZE_16KB;
-    MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-    MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-    MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-    MPU_InitStruct.Number = MPU_REGION_NUMBER3;
-    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-    MPU_InitStruct.SubRegionDisable = 0x00;
-    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
-
-    HAL_MPU_ConfigRegion(&MPU_InitStruct);
 #endif
 
     /* Configure the MPU attributes as WT for QSPI */
@@ -92,7 +76,7 @@ int mpu_init(void)
     MPU_InitStruct.IsBufferable      = MPU_ACCESS_NOT_BUFFERABLE;
     MPU_InitStruct.IsCacheable       = MPU_ACCESS_CACHEABLE;
     MPU_InitStruct.IsShareable       = MPU_ACCESS_NOT_SHAREABLE;
-    MPU_InitStruct.Number            = MPU_REGION_NUMBER4;
+    MPU_InitStruct.Number            = MPU_REGION_NUMBER3;
     MPU_InitStruct.TypeExtField      = MPU_TEX_LEVEL0;
     MPU_InitStruct.SubRegionDisable  = 0X00;
     MPU_InitStruct.DisableExec       = MPU_INSTRUCTION_ACCESS_ENABLE;
