6f709399ff1f7c7a7f449ed62b4558dc429c24a1
wanghaijing <whj4674672@163.com>
2020-11-06 16:13:33 +0800

    【修正】mpu 配置 flash 容量错误

--- projects/art_pi_net_player/board/drv_mpu.c
+++ projects/art_pi_net_player/board/drv_mpu.c
@@ -87,7 +87,7 @@ int mpu_init(void)
     /* Configure the MPU attributes as WT for QSPI */
     MPU_InitStruct.Enable            = MPU_REGION_ENABLE;
     MPU_InitStruct.BaseAddress       = 0x90000000;
-    MPU_InitStruct.Size              = MPU_REGION_SIZE_512KB;
+    MPU_InitStruct.Size              = MPU_REGION_SIZE_8MB;
     MPU_InitStruct.AccessPermission  = MPU_REGION_FULL_ACCESS;
     MPU_InitStruct.IsBufferable      = MPU_ACCESS_NOT_BUFFERABLE;
     MPU_InitStruct.IsCacheable       = MPU_ACCESS_CACHEABLE;
