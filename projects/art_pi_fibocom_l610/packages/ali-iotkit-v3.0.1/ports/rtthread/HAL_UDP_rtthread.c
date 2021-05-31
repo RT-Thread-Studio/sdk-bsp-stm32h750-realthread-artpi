7411a73424fe38b7294db72dd967ecd2eb1319fc
zhangyang_fibocom <360959574@qq.com>
2021-05-31 11:51:40 +0800

    add art_pi_fibocom_l610

---
+++ projects/art_pi_fibocom_l610/packages/ali-iotkit-v3.0.1/ports/rtthread/HAL_UDP_rtthread.c
@@ -0,0 +1,391 @@
+/*
+ * Copyright (c) 2006-2018 RT-Thread Development Team. All rights reserved.
+ * License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Again edit by rt-thread group
+ * Change Logs:
+ * Date          Author          Notes
+ * 2019-07-21    MurphyZhao      first edit
+ */
+
+#include <rtthread.h>
+#include "infra_config.h"
+
+#if defined(HAL_UDP)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define DBG_TAG                        "ali.udp"
+#define DBG_LVL                        DBG_INFO
+#include <rtdbg.h>
+
+#include "infra_compat.h"
+
+intptr_t HAL_UDP_create(char *host, unsigned short port)
+{
+#define NETWORK_ADDR_LEN    (16)
+
+    int                     rc = -1;
+    long                    socket_id = -1;
+    char                    port_ptr[6] = {0};
+    struct addrinfo         hints;
+    char                    addr[NETWORK_ADDR_LEN] = {0};
+    struct addrinfo        *res, *ainfo;
+    struct sockaddr_in     *sa = NULL;
+
+    if (NULL == host) {
+        return (-1);
+    }
+
+    sprintf(port_ptr, "%u", port);
+    memset((char *)&hints, 0x00, sizeof(hints));
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_INET;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    rc = getaddrinfo(host, port_ptr, &hints, &res);
+    if (0 != rc) {
+        LOG_E("getaddrinfo error");
+        return (-1);
+    }
+
+    for (ainfo = res; ainfo != NULL; ainfo = ainfo->ai_next) {
+        if (AF_INET == ainfo->ai_family) {
+            sa = (struct sockaddr_in *)ainfo->ai_addr;
+            inet_ntop(AF_INET, &sa->sin_addr, addr, NETWORK_ADDR_LEN);
+            fprintf(stderr, "The host IP %s, port is %d\r\n", addr, ntohs(sa->sin_port));
+
+            socket_id = socket(ainfo->ai_family, ainfo->ai_socktype, ainfo->ai_protocol);
+            if (socket_id < 0) {
+                LOG_E("create socket error");
+                continue;
+            }
+            if (0 == connect(socket_id, ainfo->ai_addr, ainfo->ai_addrlen)) {
+                break;
+            }
+
+            close(socket_id);
+        }
+    }
+    freeaddrinfo(res);
+
+    return socket_id;
+
+#undef NETWORK_ADDR_LEN
+}
+
+void HAL_UDP_close(intptr_t p_socket)
+{
+    long socket_id = -1;
+
+    socket_id = p_socket;
+    close(socket_id);
+}
+
+int HAL_UDP_write(intptr_t p_socket,
+                  const unsigned char *p_data,
+                  unsigned int datalen)
+{
+    int             rc = -1;
+    long            socket_id = -1;
+
+    socket_id = (long)p_socket;
+    rc = send(socket_id, (char *)p_data, (int)datalen, 0);
+    if (-1 == rc) {
+        return -1;
+    }
+
+    return rc;
+}
+
+
+int HAL_UDP_readTimeout(intptr_t p_socket,
+                        unsigned char *p_data,
+                        unsigned int datalen,
+                        unsigned int timeout)
+{
+    int                 ret;
+    struct timeval      tv;
+    fd_set              read_fds;
+    long                socket_id = -1;
+
+    if (0 == p_socket || NULL == p_data) {
+        return -1;
+    }
+    socket_id = (long)p_socket;
+
+    if (socket_id < 0) {
+        return -1;
+    }
+
+    FD_ZERO(&read_fds);
+    FD_SET(socket_id, &read_fds);
+
+    tv.tv_sec  = timeout / 1000;
+    tv.tv_usec = (timeout % 1000) * 1000;
+
+    ret = select(socket_id + 1, &read_fds, NULL, NULL, timeout == 0 ? NULL : &tv);
+
+    /* Zero fds ready means we timed out */
+    if (ret == 0) {
+        return -2;    /* receive timeout */
+    }
+
+    if (ret < 0) {
+        if (errno == EINTR) {
+            return -3;    /* want read */
+        }
+
+        return -4; /* receive failed */
+    }
+
+    /* This call will not block */
+    return read(p_socket, p_data, datalen);
+}
+
+intptr_t HAL_UDP_create_without_connect(const char *host, unsigned short port)
+{
+    struct sockaddr_in addr;
+    long sockfd;
+    int opt_val = 1;
+    struct hostent *hp;
+    struct in_addr in;
+    uint32_t ip;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        LOG_E("socket error");
+        return -1;
+    }
+    if (0 == port) {
+        return (intptr_t)sockfd;
+    }
+
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+
+    if (0 != setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_BROADCAST, &opt_val, sizeof(opt_val))) {
+        LOG_E("setsockopt error");
+        close(sockfd);
+        return -1;
+    }
+
+    if (NULL == host) {
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    } else {
+        if (inet_aton(host, &in)) {
+            ip = *(uint32_t *)&in;
+        } else {
+            hp = gethostbyname(host);
+            if (!hp) {
+                LOG_E("can't resolute the host address");
+                close(sockfd);
+                return -1;
+            }
+            ip = *(uint32_t *)(hp->h_addr);
+        }
+        addr.sin_addr.s_addr = ip;
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+
+    if (-1 == bind(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in))) {
+        close(sockfd);
+        return -1;
+    }
+    LOG_D("success to establish udp, fd=%d", (int)sockfd);
+
+    return (intptr_t)sockfd;
+}
+
+int HAL_UDP_connect(intptr_t sockfd,
+                    const char *host,
+                    unsigned short port)
+{
+    int                     rc = -1;
+    char                    port_ptr[6] = {0};
+    struct addrinfo         hints;
+    struct addrinfo        *res, *ainfo;
+
+    if (NULL == host) {
+        return -1;
+    }
+
+    LOG_D("HAL_UDP_connect, host=%s, port=%d", host, port);
+    sprintf(port_ptr, "%u", port);
+    memset((char *)&hints, 0x00, sizeof(hints));
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_INET;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    rc = getaddrinfo(host, port_ptr, &hints, &res);
+    if (0 != rc) {
+        LOG_E("getaddrinfo error");
+        return -1;
+    }
+
+    for (ainfo = res; ainfo != NULL; ainfo = ainfo->ai_next) {
+        if (AF_INET == ainfo->ai_family) {
+            if (0 == connect(sockfd, ainfo->ai_addr, ainfo->ai_addrlen)) {
+                freeaddrinfo(res);
+                return 0;
+            }
+        }
+    }
+    freeaddrinfo(res);
+
+    return -1;
+}
+
+int HAL_UDP_close_without_connect(intptr_t sockfd)
+{
+    return close((int)sockfd);
+}
+
+int HAL_UDP_joinmulticast(intptr_t sockfd,
+                          char *p_group)
+{
+    int err = -1;
+    int socket_id = -1;
+    int loop = 0;
+    struct ip_mreq mreq;
+
+    if (NULL == p_group) {
+        return -1;
+    }
+
+    /*set loopback*/
+    socket_id = (int)sockfd;
+    err = setsockopt(socket_id, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop));
+    if (err < 0) {
+        LOG_E("setsockopt error");
+        return err;
+    }
+
+    mreq.imr_multiaddr.s_addr = inet_addr(p_group);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY); /*default networt interface*/
+
+    /*join to the multicast group*/
+    err = setsockopt(socket_id, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
+    if (err < 0) {
+        LOG_E("setsockopt error");
+        return err;
+    }
+
+    return 0;
+}
+
+int HAL_UDP_recvfrom(intptr_t sockfd,
+                     NetworkAddr *p_remote,
+                     unsigned char *p_data,
+                     unsigned int datalen,
+                     unsigned int timeout_ms)
+{
+    int ret;
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+    fd_set read_fds;
+    struct timeval timeout = {timeout_ms / 1000, (timeout_ms % 1000) * 1000};
+
+    FD_ZERO(&read_fds);
+    FD_SET(sockfd, &read_fds);
+
+    ret = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
+    if (ret == 0) {
+        return 0;    /* receive timeout */
+    }
+
+    if (ret < 0) {
+        if (errno == EINTR) {
+            return -3;    /* want read */
+        }
+        return -4; /* receive failed */
+    }
+
+    ret = recvfrom(sockfd, p_data, datalen, 0, (struct sockaddr *)&addr, &addr_len);
+    if (ret > 0) {
+        if (NULL != p_remote) {
+            p_remote->port = ntohs(addr.sin_port);
+
+            strcpy((char *)p_remote->addr, inet_ntoa(addr.sin_addr));
+        }
+
+        return ret;
+    }
+
+    return -1;
+}
+
+int HAL_UDP_sendto(intptr_t sockfd,
+                   const NetworkAddr *p_remote,
+                   const unsigned char *p_data,
+                   unsigned int datalen,
+                   unsigned int timeout_ms)
+{
+    int ret;
+    uint32_t ip;
+    struct in_addr in;
+    struct hostent *hp;
+    struct sockaddr_in addr;
+    fd_set write_fds;
+    struct timeval timeout = {timeout_ms / 1000, (timeout_ms % 1000) * 1000};
+
+    if (inet_aton((char *)p_remote->addr, &in)) {
+        ip = *(uint32_t *)&in;
+    } else {
+        hp = gethostbyname((char *)p_remote->addr);
+        if (!hp) {
+            LOG_E("can't resolute the host address");
+            return -1;
+        }
+        ip = *(uint32_t *)(hp->h_addr);
+    }
+
+    FD_ZERO(&write_fds);
+    FD_SET(sockfd, &write_fds);
+
+    ret = select(sockfd + 1, NULL, &write_fds, NULL, &timeout);
+    if (ret == 0) {
+        return 0;    /* write timeout */
+    }
+
+    if (ret < 0) {
+        if (errno == EINTR) {
+            return -3;    /* want write */
+        }
+        return -4; /* write failed */
+    }
+
+    addr.sin_addr.s_addr = ip;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(p_remote->port);
+
+    ret = sendto(sockfd, p_data, datalen, 0, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+
+    if (ret < 0) {
+        LOG_E("sendto error");
+    }
+
+    return (ret) > 0 ? ret : -1;
+}
+
+#endif  /* #if defined(HAL_UDP) */
---
+++ projects/art_pi_fibocom_l610/rtconfig.h
@@ -0,0 +1,284 @@
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread Configuration */
+
+/* RT-Thread Kernel */
+
+#define RT_NAME_MAX 8
+#define RT_ALIGN_SIZE 4
+#define RT_THREAD_PRIORITY_32
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 1000
+#define RT_USING_OVERFLOW_CHECK
+#define RT_USING_HOOK
+#define RT_USING_IDLE_HOOK
+#define RT_IDLE_HOOK_LIST_SIZE 4
+#define IDLE_THREAD_STACK_SIZE 256
+#define RT_USING_TIMER_SOFT
+#define RT_TIMER_THREAD_PRIO 4
+#define RT_TIMER_THREAD_STACK_SIZE 512
+#define RT_DEBUG
+
+/* Inter-Thread communication */
+
+#define RT_USING_SEMAPHORE
+#define RT_USING_MUTEX
+#define RT_USING_EVENT
+#define RT_USING_MAILBOX
+#define RT_USING_MESSAGEQUEUE
+
+/* Memory Management */
+
+#define RT_USING_MEMPOOL
+#define RT_USING_MEMHEAP
+#define RT_USING_MEMHEAP_AS_HEAP
+#define RT_USING_HEAP
+
+/* Kernel Device Object */
+
+#define RT_USING_DEVICE
+#define RT_USING_CONSOLE
+#define RT_CONSOLEBUF_SIZE 128
+#define RT_CONSOLE_DEVICE_NAME "uart4"
+#define RT_VER_NUM 0x40003
+#define ARCH_ARM
+#define RT_USING_CPU_FFS
+#define ARCH_ARM_CORTEX_M
+#define ARCH_ARM_CORTEX_M7
+
+/* RT-Thread Components */
+
+#define RT_USING_COMPONENTS_INIT
+#define RT_USING_USER_MAIN
+#define RT_MAIN_THREAD_STACK_SIZE 2048
+#define RT_MAIN_THREAD_PRIORITY 10
+
+/* C++ features */
+
+
+/* Command shell */
+
+#define RT_USING_FINSH
+#define FINSH_THREAD_NAME "tshell"
+#define FINSH_USING_HISTORY
+#define FINSH_HISTORY_LINES 5
+#define FINSH_USING_SYMTAB
+#define FINSH_USING_DESCRIPTION
+#define FINSH_THREAD_PRIORITY 20
+#define FINSH_THREAD_STACK_SIZE 4096
+#define FINSH_CMD_SIZE 80
+#define FINSH_USING_MSH
+#define FINSH_USING_MSH_DEFAULT
+#define FINSH_ARG_MAX 10
+
+/* Device virtual file system */
+
+#define RT_USING_DFS
+#define DFS_USING_WORKDIR
+#define DFS_FILESYSTEMS_MAX 2
+#define DFS_FILESYSTEM_TYPES_MAX 2
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_DEVFS
+
+/* Device Drivers */
+
+#define RT_USING_DEVICE_IPC
+#define RT_PIPE_BUFSZ 512
+#define RT_USING_SYSTEM_WORKQUEUE
+#define RT_SYSTEM_WORKQUEUE_STACKSIZE 2048
+#define RT_SYSTEM_WORKQUEUE_PRIORITY 23
+#define RT_USING_SERIAL
+#define RT_SERIAL_RB_BUFSZ 4096
+#define RT_USING_PIN
+#define RT_USING_RTC
+#define RT_USING_SPI
+
+/* Using USB */
+
+
+/* POSIX layer and C standard library */
+
+#define RT_USING_LIBC
+#define RT_USING_POSIX
+
+/* Network */
+
+/* Socket abstraction layer */
+
+#define RT_USING_SAL
+
+/* protocol stack implement */
+
+#define SAL_USING_AT
+#define SAL_USING_TLS
+#define SAL_USING_POSIX
+
+/* Network interface device */
+
+#define RT_USING_NETDEV
+#define NETDEV_USING_IFCONFIG
+#define NETDEV_USING_PING
+#define NETDEV_USING_NETSTAT
+#define NETDEV_USING_AUTO_DEFAULT
+#define NETDEV_IPV4 1
+#define NETDEV_IPV6 0
+
+/* light weight TCP/IP stack */
+
+
+/* AT commands */
+
+#define RT_USING_AT
+#define AT_DEBUG
+#define AT_USING_CLIENT
+#define AT_CLIENT_NUM_MAX 1
+#define AT_USING_SOCKET
+#define AT_USING_CLI
+#define AT_PRINT_RAW_CMD
+#define AT_CMD_MAX_LEN 4096
+#define AT_SW_VERSION_NUM 0x10300
+
+/* VBUS(Virtual Software BUS) */
+
+
+/* Utilities */
+
+
+/* RT-Thread online packages */
+
+/* IoT - internet of things */
+
+#define PKG_USING_WEBCLIENT
+#define WEBCLIENT_DEBUG
+#define WEBCLIENT_USING_SAMPLES
+#define WEBCLIENT_NOT_USE_TLS
+#define PKG_USING_WEBCLIENT_V212
+#define PKG_WEBCLIENT_VER_NUM 0x20102
+#define PKG_USING_CJSON
+#define PKG_USING_CJSON_V102
+
+/* Wi-Fi */
+
+/* Marvell WiFi */
+
+
+/* Wiced WiFi */
+
+#define PKG_USING_AT_DEVICE
+#define AT_DEVICE_USING_L610
+#define AT_DEVICE_L610_INIT_ASYN
+#define AT_DEVICE_L610_SAMPLE
+#define L610_SAMPLE_POWER_PIN 133
+#define L610_SAMPLE_STATUS_PIN -1
+#define L610_SAMPLE_CLIENT_NAME "uart1"
+#define L610_SAMPLE_RECV_BUFF_LEN 4096
+#define PKG_USING_AT_DEVICE_LATEST_VERSION
+#define PKG_AT_DEVICE_VER_NUM 0x99999
+
+/* IoT Cloud */
+
+#define PKG_USING_ALI_IOTKIT
+#define PKG_USING_ALI_IOTKIT_PRODUCT_KEY "a1nV4jvMOUh"
+#define PKG_USING_ALI_IOTKIT_PRODUCT_SECRET "rSkpv88o3IoBA7YQ"
+#define PKG_USING_ALI_IOTKIT_DEVICE_NAME "867567041091141"
+#define PKG_USING_ALI_IOTKIT_DEVICE_SECRET "733777cce83a906df4649e5d40dea9c2"
+#define PKG_ALI_IOTKIT_SELECT_ITEM
+#define PLATFORM_HAS_STDINT
+#define PLATFORM_HAS_DYNMEM
+#define PLATFORM_HAS_OS
+#define INFRA_STRING
+#define INFRA_NET
+#define INFRA_LIST
+#define INFRA_LOG_NETWORK_PAYLOAD
+#define INFRA_LOG
+
+/* Log Configurations */
+
+#define INFRA_LOG_MUTE_FLW
+#define INFRA_LOG_MUTE_DBG
+#define INFRA_LOG_MUTE_INF
+#define INFRA_LOG_MUTE_WRN
+#define INFRA_LOG_MUTE_CRT
+#define INFRA_TIMER
+#define INFRA_CJSON
+#define INFRA_SHA256
+#define INFRA_REPORT
+#define INFRA_COMPAT
+#define INFRA_CLASSIC
+#define DEV_SIGN
+#define MQTT_COMM_ENABLED
+
+/* MQTT Configurations */
+
+#define MQTT_DEFAULT_IMPL
+#define MQTT_DIRECT
+#define DEVICE_MODEL_CLASSIC
+#define LOG_REPORT_TO_CLOUD
+#define DEVICE_MODEL_ENABLED
+
+/* Device Model Configurations */
+
+#define SUPPORT_TLS
+#define PKG_USING_ALI_IOTKIT_SAMPLES
+#define PKG_USING_ALI_IOTKIT_MQTT_SAMPLE
+#define PKG_USING_ALI_IOTKIT_V30001
+#define PKG_ALI_IOTKIT_VER_NUM 0x30001
+
+/* security packages */
+
+#define PKG_USING_MBEDTLS
+
+/* Select Root Certificate */
+
+#define MBEDTLS_AES_ROM_TABLES
+#define MBEDTLS_ECP_WINDOW_SIZE 2
+#define MBEDTLS_SSL_MAX_CONTENT_LEN 8192
+#define PKG_USING_MBEDTLS_LATEST_VERSION
+
+/* language packages */
+
+
+/* multimedia packages */
+
+
+/* tools packages */
+
+
+/* system packages */
+
+
+/* peripheral libraries and drivers */
+
+
+/* miscellaneous packages */
+
+
+/* samples: kernel and components samples */
+
+
+/* Hardware Drivers Config */
+
+#define SOC_STM32H750XB
+#define SOC_SERIES_STM32H7
+
+/* Board extended module */
+
+
+/* Onboard Peripheral */
+
+#define BSP_USING_USB_TO_USART
+
+/* On-chip Peripheral */
+
+#define BSP_USING_GPIO
+#define BSP_USING_UART
+#define BSP_USING_UART1
+#define BSP_USING_UART4
+
+/* External Libraries */
+
+#define RT_STUDIO_BUILT_IN
+
+#endif
