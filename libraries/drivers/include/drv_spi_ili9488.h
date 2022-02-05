b82aac394bc7d466bbf7d7935f19ea51d8cf7de4
luhuadong <luhuadong@163.com>
2022-02-05 18:30:14 +0800

    update ili9488 driver

--- libraries/drivers/include/drv_spi_ili9488.h
+++ libraries/drivers/include/drv_spi_ili9488.h
@@ -1,18 +1,22 @@
 /*
- * Copyright (c) 2006-2018, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author            Notes
- * 2018-08-14    flybreak           the first version
- * 2018-09-18    balanceTWK         add sleep mode function 
+ * 2018-08-14     flybreak          the first version
+ * 2018-09-18     balanceTWK        add sleep mode function
+ * 2022-02-01     Rudy Lo           add lcd_fill_array function
  */
 
 #ifndef __DRV_LCD_SPI_H__
 #define __DRV_LCD_SPI_H__
 
 #include <rtthread.h>
+
+#define LCD_W 480
+#define LCD_H 320
 
 //POINT_COLOR
 #define WHITE            0xFFFFFF
@@ -46,6 +50,7 @@ void lcd_draw_circle(rt_uint16_t x0, rt_uint16_t y0, rt_uint8_t r);
 void lcd_draw_line(rt_uint16_t x1, rt_uint16_t y1, rt_uint16_t x2, rt_uint16_t y2);
 void lcd_draw_rectangle(rt_uint16_t x1, rt_uint16_t y1, rt_uint16_t x2, rt_uint16_t y2);
 void lcd_fill(rt_uint16_t x_start, rt_uint16_t y_start, rt_uint16_t x_end, rt_uint16_t y_end, rt_uint32_t color);
+void lcd_fill_array(rt_uint16_t x_start, rt_uint16_t y_start, rt_uint16_t x_end, rt_uint16_t y_end, void *pcolor);
 
 void lcd_show_num(rt_uint16_t x, rt_uint16_t y, rt_uint32_t num, rt_uint8_t len, rt_uint32_t size);
 rt_err_t lcd_show_string(rt_uint16_t x, rt_uint16_t y, rt_uint32_t size, const char *fmt, ...);
